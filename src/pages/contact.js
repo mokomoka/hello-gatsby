54300af50cceb38144e16b19e79e62b8ffc79964
Tomoka Hayashi <g2120042@fun.ac.jp>
2020-05-21 21:26:06 +0900

    Add features at tutorial3

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default function Contact() {
   return (
-    <div style={{ color: `teal` }}>
-      <Link to="/">Home</Link>
-      <Header headerText="Contact" />
-      <p>Send us a message!</p>
-    </div>
+    <Layout>
+      <h1>I'd love to talk! Email me at the address below</h1>
+      <p>
+        <a href="mailto:me@example.com">me@example.com</a>
+      </p>
+    </Layout>
   )
 }
